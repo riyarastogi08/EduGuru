0d3c5a32215450758dbdab058833856bfbe0d633
riyarastogi08 <riyarastogi8892@gmail.com>
2024-05-14 11:23:39 +0530

    update

--- Frontend/src/Components/Main/Navbar.jsx
+++ Frontend/src/Components/Main/Navbar.jsx
@@ -112,6 +112,20 @@ const Navbar = () => {
           College
         </a>
       </li>
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          className="w-4 h-4 current-fill"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+          />
+        </svg>
   
       <li>
         <a className="text-sm text-blue-900 text-xl font-bold hover:text-blue-700" href="/Main/Contact">
